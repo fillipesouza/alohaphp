e32aded68205e46d6be06fa4d8de181d34c5dcdd
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2019-10-11 22:39:47 -0300

    Colocando o metodo de leitura de cursos no banco

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 include_once("src/helpers/banco.php");
-include_once("/src/models/curso.php");
-include_once("/src/models/cursoDAO.php");
+include_once("src/models/cursoDAO.php");
 
 
 $dao = new CursoDAO($pdo);
 $curso = new Curso(0, "Cozinha", 24);
-$dao->save($curso);
+//$dao->save($curso);
 
+$results = $dao->readAll();
+print_r($results);
 
 /*
 $sql = "SELECT * FROM tb_cursos";
--- src/models/cursoDAO.php
+++ src/models/cursoDAO.php
@@ -1,4 +1,5 @@
 <?php 
+require_once('src/models/curso.php');
 class CursoDAO {
     private $con;
     function __construct($con){
@@ -8,6 +9,13 @@ class CursoDAO {
         $sql = "INSERT INTO tb_cursos (nome, professor) values (?,?)";
         $stmt = $this->con->prepare($sql);
         $stmt->execute(array($curso->nome, $curso->professor));
+    }
+
+    function readAll(){
+        $stmt = $this->con->prepare('SELECT * FROM tb_cursos');
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, "Curso", array('curso_id', 'nome', 'professor'));
+        return $results;
     }
 }
 
