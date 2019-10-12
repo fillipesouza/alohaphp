187b8231470148d97698a7ac49745a61836590e5
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2019-10-11 22:33:20 -0300

    Colocando construtor no curso

--- src/models/curso.php
+++ src/models/curso.php
@@ -1,6 +1,10 @@
 <?php 
 class Curso {
   public $curso_id, $nome, $professor;
-  public static $params = array('nome', 'professor');
+  function __construct($curso_id, $nome, $professor){
+    $this->curso_id = $curso_id;
+    $this->nome = $nome;
+    $this->professor = $professor;
+  }
 }
 ?>
