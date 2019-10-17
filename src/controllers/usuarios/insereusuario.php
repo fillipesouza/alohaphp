bf5bd50b67b3504e5b5de1aaaef91bba43dca375
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2019-10-16 23:00:03 -0300

    Criando o primeiro controlador - insereusuario.php

---
+++ src/controllers/usuarios/insereusuario.php
@@ -0,0 +1,14 @@
+<?php
+ require_once "../../helpers/banco.php";
+ require_once "../../models/usuarioDAO.php";
+ $usuarioDAO = new UsuarioDAO($pdo);
+ $rm = $_POST["rm"];
+ $nome = $_POST["nome"];
+ $senha = $_POST["senha"];
+ $curso = $_POST["curso"];
+ 
+ $usuario = Usuario::criaUsuario($rm,$nome,$curso);
+ $usuario->setarSenha($senha);
+ $usuarioDAO->save($usuario);
+ echo("Usuario inserido");
+?>
--- src/models/usuarioDAO.php
+++ src/models/usuarioDAO.php
@@ -12,6 +12,21 @@ class UsuarioDAO {
         $stmt = $this->con->prepare($sql);
         $stmt->execute([$usuario->rm, $usuario->nome, $usuario->senha, $usuario->curso]);
     }
+
+    function delete($usuario){
+        $sql = "DELETE FROM ".$this->tabela. " where rm=?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute([$usuario->rm]);
+    }
+
+    function readAll($usuario){
+        $sql = "SELECT * FROM ".$this->tabela;
+        $stmt = $this->con->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_CLASS, "Usuario");
+        return $results;
+        
+    }
 }
 
 ?>
