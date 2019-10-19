7a2f8423d9328d059be8c66674d98a6243aa1a78
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2019-10-18 22:25:59 -0300

    Adicionando o painel administrativo

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
 <?php
-include_once("src/helpers/banco.php");
-include_once("src/models/cursoDAO.php");
-include_once("src/models/usuarioDAO.php");
-
-
-/*$dao = new CursoDAO($pdo);
-$curso = Curso::criaCurso(1,"Informatica",58);
-$dao->save($curso);
-*/
-$usuarioDAO = new UsuarioDAO($pdo);
-$usuario = Usuario::criaUsuario(183,"Estefani",8);
-$usuario->setarSenha("Estefani183");
-$usuarioDAO->save($usuario);
-
-
+session_start();
 ?>
+<?php include_once('src/models/usuario.php'); ?>
+<?php
+$listaDeUsuarios = array();
+$usuario1 = Usuario::criaUsuario(24,"Mat Costa","Informática");
+$usuario2 = Usuario::criaUsuario(38,"Felipe Aug","Informática");
+array_push($listaDeUsuarios, $usuario1);
+array_push($listaDeUsuarios, $usuario2);
+?>
+<? include_once "partials/head.php"; ?>
+
+<table class="table table-striped table-inverse table-responsive">
+    <thead class="thead-inverse">
+        <tr>
+            <th>RM</th>
+            <th>NOME</th>
+            <th>CURSO</th>
+            <th>EDITAR</th>
+            <th>REMOVER</th>
+        </tr>
+        </thead>
+        <tbody>
+            <?php foreach($listaDeUsuarios as $usuario){ ?>
+                 <tr>
+                    <td><?=$usuario->rm ?></td>
+                    <td><?=$usuario->nome ?></td>
+                    <td><?=$usuario->curso ?></td>
+                    <td> edit </td>
+                    <td> delete </td>
+                 </tr>
+            <?php } ?>
+        </tbody>
+        <tfoot>
+        <tr><td colspan="5" ><button type="button" class="btn btn-primary">CRIAR USUARIO</button> </td></tr>
+        </tfoot>
+</table>
+
+<? include_once "partials/foot.php"; ?>
